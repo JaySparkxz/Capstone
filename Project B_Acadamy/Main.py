1666dfa6ce08d63e784a50d616ab616f4cdc0a70
JaySpark <JustinConcepc@gmail.com>
2024-02-09 10:39:25 -0500

    Added documentation

--- Project B_Acadamy/Main.py
+++ Project B_Acadamy/Main.py
@@ -1,20 +1,29 @@
 import csv
 import math 
 
+
+# ===== variables ======
 TotalQuestions = 0
 Goal = .75
 WrongAnswers = 0
 CorrectAnswers = 0    
+# ===== =====
 
+
+# Opens a csv file with questions and answers. Currently set up to add two numbers in two seperate columns 
 with open('./QSHort.csv', newline='') as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader: 
         TotalQuestions += 1
+
+# Calcs Maxiomum number of wrong answers 
 MaxWrongAnswers = math.ceil(TotalQuestions-(TotalQuestions*Goal))
+
         
 with open('./QSHort.csv', newline='') as csvFile:
     reader = csv.DictReader(csvFile)
-    print(f'MaxWrongAnswers: {MaxWrongAnswers}')
+    
+    # print(f'MaxWrongAnswers: {MaxWrongAnswers}') Check to make sure MaxWrongAnswers is an int. 
     for row in reader:
         if MaxWrongAnswers > WrongAnswers:
             answer = int(input(f"{row['Num1']} + {row['Num2']} = "))
